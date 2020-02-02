e974de7b166b8978084a25d1d48c8c7a756a3c3a
saba756 <sabaofficial12@gmail.com>
2020-02-02 20:03:03 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { HttpClient } from 'selenium-webdriver/http';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      AppRoutingModule,
+      FormsModule,
+      BsDropdownModule.forRoot()
+   ],
+   providers: [
+      AuthService, ErrorInterceptorProvider
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+ model: any ={};
+  constructor(public authService: AuthService ,
+              private alertify: AlertifyService) { }
+
+  ngOnInit() {
+  }
+  login(){
+    this.authService.login(this.model).subscribe(next =>{
+     this.alertify.success('login successfully');
+    }, error =>{
+      this.alertify.error(error);
+     
+    });
+  }
+  loggedIn(){
+      // const token = localStorage.getItem('token');
+      // return !! token;
+      return this.authService.loggedIn();
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('logout');
+  }
+}
